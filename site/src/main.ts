25f5fd3be7f8e418b5e30b9b08d861265f8f42fa
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2025-05-16 22:52:58 -0300

    feat: site base

--- site/src/main.ts
+++ site/src/main.ts
@@ -1,28 +1,11 @@
 // @ts-ignore
-import './style.css'
-// @ts-ignore
-import typescriptLogo from './typescript.svg'
+import './styles/global.css'
 // @ts-ignore
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-    <p>Teste 2, agora vai 2, to ceto.</p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  <main>
+    <h1> Hello Word !!! </h1>
+  </main>
+`;
