46507707db014e99cb090578a57e677012cfadc2
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2025-05-17 10:58:53 -0300

    test: csm service moke

--- site/src/controllers/cms.controller.ts
+++ site/src/controllers/cms.controller.ts
@@ -1,11 +1,30 @@
 import { cmsComponent } from "../components/cms.component";
 import { cmsService } from "../services/cms.service";
-import type { Article } from "../types";
+import type { AllArticles, Article } from "../types";
+
 
 export async function cmsController() {
-    const allArticles: Array<Article> = await cmsService()
+    // const allArticles: Array<Article> = await cmsService()
 
-    allArticles.map((article: Article) => {
+    allArticles().map((article: Article) => {
         cmsComponent(article);
     })
+}
+
+function allArticles(): AllArticles {
+    const article1: Article = {
+        title: "Meu artigo 1",
+        description: "# Meu artigo 1"
+    }
+
+    const article2: Article = {
+        title: "Meu artigo 2",
+        description: "# meu artigo 2"
+    }
+
+    const allArticles: AllArticles =[
+        article1, article2
+    ];
+    
+    return allArticles
 }
