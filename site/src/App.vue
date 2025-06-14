b8bfb653bfa4d6ac82d3c7441cb5861de1f50a5e
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2025-06-14 13:20:12 -0300

    diretivas

--- site/src/App.vue
+++ site/src/App.vue
@@ -1,7 +1,32 @@
 <script setup lang="ts">
 import HeaderComponent from '@/components/HeaderComponent.vue';
+import { ref } from 'vue';
+
+const showHeader = ref<boolean>(false);
+const user = ref({
+  firstName: 'john',
+  lastName: 'doe'
+})
+
 </script>
 
 <template>
-  <HeaderComponent />
+  <main>
+    <HeaderComponent v-show="showHeader"/>
+    <h1>Teste</h1>
+    <p>{{ user.firstName }}</p>
+    <p>{{ user.lastName }}</p>
+  </main>
 </template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: #f0f0f0;
+}
+</style>
